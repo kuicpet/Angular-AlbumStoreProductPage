8011c22fc3363b15b10f04c04148149bc022507b
Kingsley <kingsleyumujeyan@gmail.com>
2019-10-26 22:28:37 +0100

    Module Completed

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,7 +9,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductListComponent implements OnInit {
 
-  product: Product[];
+  products: Product[];
 
   constructor(private _ProductService: ProductService) { }
 
