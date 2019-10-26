d338ef9cde3479b247b66135a42f5e0834cef852
Kingsley <kingsleyumujeyan@gmail.com>
2019-10-26 21:31:07 +0100

    Module Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from './product.service';
 import { ProductTracklisting } from './product-tracklisting./product-tracklisting..component';
+import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
     ProductDescriptionComponent,
-    ProductTracklisting.Component
+    ProductTracklisting.Component,
+    ProductTracklistingComponent
   ],
   imports: [
     BrowserModule,
